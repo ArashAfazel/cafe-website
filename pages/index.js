7e926de1dbaf6b121fbb8883c5b895b40e13aa2d
arash <arashafazel8@gmail.com>
2026-07-31 19:37:58 +0330

    Add Slider module

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Slider from "@/components/modules/Slider/Slider";
+import About from "@/components/modules/About/About"
+import Categories from "@/components/modules/Categories/Categories";
 export default function Home() {
   return (
     <>
-
+    
+    <Slider/>
+    <About/>
+    <Categories/>
     </>
-  );
+  )
 }
-
