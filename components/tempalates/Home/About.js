ffd2cf0805f105e837da69c1f855f653ae5d5864
arash <arashafazel8@gmail.com>
2026-08-15 20:50:05 +0330

    feat(Home) Add About us section

---
+++ components/tempalates/Home/About.js
@@ -0,0 +1,33 @@
+export default function About() {
+  return (
+    <>
+      {/* content */}
+      <div  className="w-full ">
+        <div className="w-full flex justify-center pt-3">
+          <span className="bg-amber-500 w-0.5 h-15 md:h-25"></span>
+        </div>
+        <div className="flex justify-center">
+          <span className="text-[15px] md:text-[30px] text-amber-500">
+            About Us
+          </span>
+        </div>
+        <div className="lg:flex justify-between pt-4 items-center px-3.5 text-center">
+          <span className="lg:w-[40%] lg:text-[17px] md:text-[15px] text-[10px]  text-center lg:font-bold">
+            we believe that every cup of coffee tells a story. From the
+            sun-drenched hills of Ethiopia to the farms in Colombia, we source
+            the highest-quality beans from around the globe Our mission is
+            simple to provide exceptional coffee experiences for every sip. 
+          </span>
+          <img className="max-sm:w-30 sm:w-100 max-lg:hidden" src="/images/cup.png" />
+          <span className="lg:w-[40%] lg:text-[17px] md:text-[15px] text-[10px] text-center lg:font-bold">
+             whether you're a seasoned espresso aficionado or a casual coffee
+            drinker, we have a blend that's perfect for you. Our roasting
+            process ensures that each batch is roasted to perfection, bringing
+            out the unique flavors and aromas that make coffee such an exciting
+            beverage
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ components/tempalates/Home/Slider.js
@@ -0,0 +1,89 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay } from "swiper/modules";
+import { Playfair } from "next/font/google";
+
+const playfair = Playfair({
+  subsets: ["latin"],
+  weight: "800",
+  variable: "--font-playfair",
+});
+
+export default function Slider() {
+  return (
+    <>
+      <div className="">
+        <div className="sm:hidden relative">
+          <Swiper
+            modules={[Autoplay]}
+            autoplay={{ delay: 3000 }}
+            loop={true}
+            className="mySwiper w-full h-screen "
+          >
+            <SwiperSlide>
+              <img
+                className=" object-center w-full h-full "
+                src="/images/landing-mob-1.jpg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className=" object-center w-full h-full "
+                src="/images/landing-mob-2.jpg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="  w-full h-full  "
+                src="/images/landing-mob-3.jpg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className=" object-center w-full h-full"
+                src="/images/landing-mob-4.jpg"
+              />
+            </SwiperSlide>
+          </Swiper>
+          <div className="absolute inset-0 text-white z-2 bg-black/60"></div>
+        </div>
+        <div className="max-sm:hidden relative ">
+          <Swiper
+            modules={[Autoplay]}
+            autoplay={{ delay: 3000 }}
+            loop={true}
+            className="mySwiper w-full h-screen "
+          >
+            <SwiperSlide>
+              <img
+                className=" object-center w-full h-full  "
+                src="/images/landing1.jpg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className=" object-center w-full h-full "
+                src="/images/landing2.jpg"
+              />
+            </SwiperSlide>
+          </Swiper>
+          <div className="absolute inset-0 text-white z-2 bg-black/60"></div>
+        </div>
+        <div
+          className={` absolute top-[40%] left-[15%] text-white text-4xl md:text-7xl z-3 flex flex-col ${playfair.className}`}
+        >
+          <span>Sweet Moments</span>
+          <div>
+          <span>Start </span>
+          <span className="text-amber-700">Here</span>
+          </div>
+        </div>
+
+              <div className="w-full absolute -bottom-1 z-10">
+        <img className="w-full h-full" src="/images/overlay-top.png" />
+      </div>
+      </div>
+    </>
+  );
+}
